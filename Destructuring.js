4dced6ad0f5cbd96e7d47a4c4205205beb20ab9a
naeem <naeemislamjr182@gmail.com>
2023-09-06 03:16:05 +0600

    objecty destructuring n

--- Destructuring.js
+++ Destructuring.js
@@ -21,3 +21,43 @@ function boxing(n1, n2) {
 console.log(boxing(123, 145))
 const [one, two] = boxing(858, 3494);
 console.log(one, two)
+//////////////
+//obj destructuring
+const movie = {
+    name: 'bangla movie',
+    // age=23;
+    movies: ['king khan', 'buker opor pahar']
+
+}
+//array
+const [firstmovie, secoundmovie] = movie.movies
+console.log(`1st movie: ${firstmovie}\n 2nd movie: ${secoundmovie}`)
+//object
+const { movies, name } = {
+    name: 'bangla movie',
+    // age=23;
+    movies: ['king khan', 'buker opor pahar']
+
+}
+console.log(name, movies)
+
+//obj destructuring
+const employee = {
+    ide: 'VS Code',
+    designation: 'developer',
+    machine: 'mac',
+    languages: ['html', 'css', 'js'],
+    specification: {
+        height: 66,
+        weight: 67,
+        address: 'kumarkhali',
+        drink: 'water',
+        watch: {
+            color: 'black',
+            price: 500,
+            brand: 'garmin'
+        }
+    }
+}
+const { machine, ide } = employee
+console.log(ide, machine)
