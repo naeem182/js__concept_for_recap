7a2f8996551344eb464476d04d1a7d4d637489ba
naeem <naeemislamjr182@gmail.com>
2023-09-06 02:48:17 +0600

    get value by different types of array with destructuring

---
+++ Destructuring.js
@@ -0,0 +1,12 @@
+// 1. array destructuring
+const numbers = [42, 65, 69];
+//1
+// const x = numbers[0];
+// const y = numbers[1];
+// console.log(x, y)
+//2
+// const [x, y] = [42, 46];
+// console.log(x, y)
+//3
+const [, y] = numbers;
+console.log(y)
