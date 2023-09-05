ef757c02da6d1e2679ac9898eca4854354cf1755
naeem <naeemislamjr182@gmail.com>
2023-09-06 02:31:49 +0600

    array.findh(()=>{.....})+++includes.

--- arraymethode.js
+++ arraymethode.js
@@ -2,7 +2,7 @@ const product = [
     { name: 'laptop', price: 1213, brand: "lenevo", color: 'red' },
     { name: 'laptop', price: 5000, brand: "hp", color: 'blue' },
     { name: 'laptop', price: 5022, brand: "asus", color: 'xed' },
-    { name: 'laptop', price: 2134, brand: "lenevo", color: 'black' },
+    { name: 'phone', price: 2134, brand: "lenevo", color: 'black' },
     { name: 'laptop', price: 1366, brand: "lenevo", color: 'red' },
 ]
 //map-----array banay dey
@@ -19,4 +19,10 @@ product.forEach(product => console.log('color name by forEach', product.color))
 //multyline forEach
 product.forEach(p => {
     console.log('band name by forEach multyline', p.brand)
-})
+})//////////////////
+//filter ;;;;jara jara condition fullfill karbe sobaike dekhabe
+const _2000erbeshiPrice = product.filter(product => product.price >= 2000)
+console.log(_2000erbeshiPrice);
+///filter by include
+const specificName = product.filter(p => p.name.includes('n'))
+console.log(specificName)
